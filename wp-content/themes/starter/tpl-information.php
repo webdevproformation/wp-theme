648d577c069fcdc41778de37a5fa4e8651588fa1
malik <malik.h@webdevpro.net>
2020-07-23 16:13:18 +0200

    etape 1 boucle wp

--- wp-content/themes/starter/tpl-information.php
+++ wp-content/themes/starter/tpl-information.php
@@ -7,7 +7,14 @@ Template Name: information
 <?php get_header() ?> 
 
 <div class="container">
-    Toutes les informations sur mon activité de développeur 
+    
+    <?php while ( have_posts() ) { // requête sql + connexion
+        the_post();  // crée une variable $post est qui contient le résultat de la requête
+        echo "<pre>";
+        var_dump($post);
+        echo "</pre>";
+    } ?>
+
 </div>
 
 <?php get_footer() ?>
