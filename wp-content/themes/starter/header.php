25ad2286f1ebf61b8877289479e63884677a7ff0
malik <malik.h@webdevpro.net>
2020-07-23 14:04:49 +0200

    barre des raccourcis

--- wp-content/themes/starter/header.php
+++ wp-content/themes/starter/header.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>accueil site internet</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <?php wp_head() ?>
 </head>
 <body>
