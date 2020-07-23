d4688ea7842389b436a5dfa216784989a6d5b6f5
malik <malik.h@webdevpro.net>
2020-07-23 16:24:35 +0200

    4 fonctions principale dans la loop wordpress

--- wp-content/themes/starter/tpl-information.php
+++ wp-content/themes/starter/tpl-information.php
@@ -9,11 +9,22 @@ Template Name: information
 <div class="container">
     
     <?php while ( have_posts() ) { // requête sql + connexion
-        the_post();  // crée une variable $post est qui contient le résultat de la requête
-        echo "<pre>";
-        var_dump($post);
-        echo "</pre>";
+        the_post();  // crée une variable $post qui contient le résultat de la requête
+        // la variable $post est un objet php contient de nombreuses propriétés 
+        //echo "<pre>";
+
+        //var_dump($post->post_title);
+        the_title();
+        //echo "<br>";
+        //var_dump($post->post_content);
+        the_content();
+
+        the_author();
+
+        the_date();
+        // echo "</pre>";
     } ?>
+
 
 </div>
 
