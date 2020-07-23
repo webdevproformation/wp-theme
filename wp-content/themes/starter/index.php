49d21b288bc4ca5d1f651039b1cce5d1ad43e44b
malik <malik.h@webdevpro.net>
2020-07-23 14:16:01 +0200

    ajouté le composant navbar de bootstrap

--- wp-content/themes/starter/index.php
+++ wp-content/themes/starter/index.php
@@ -1,6 +1,25 @@
 <?php get_header() ?> <!--  ajouter l'entête de page html -->
+
+<!-- bootstrap => composant menu navbar -->
 <div class="container">
-    <h1 class="mt-5 text-primary">je suis en train de modifier le thème </h1>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item"> 
+                <a href="#" class="nav-link">Accueil</a>
+            </li>
+            <li class="nav-item"> 
+                <a href="#" class="nav-link">Présentation</a>
+            </li>
+            <li class="nav-item"> 
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+
+<div class="container">
+    <h1 class="text-primary">je suis en train de modifier le thème </h1>
     <p class="bg-success">un paragraphe ...</p>
 </div>
 <?php get_footer() ?> <!-- ajouter la fin de la page html -->
